3b702851aceedca45c3a48a58795f7945800deeb
M. Kreis <mail@mariuskreis.de>
2024-10-18 22:33:59 +0200

    Fix text color

--- ili9341.py
+++ ili9341.py
@@ -605,15 +605,15 @@ class Display(object):
         if self.is_off_grid(x, y, x + 7, y + 7):
             return
         # Rearrange color
-        r = (color & 0xF800) >> 8
-        g = (color & 0x07E0) >> 3
-        b = (color & 0x1F) << 3
+        r = (color & 0xF800) >> 11
+        g = (color & 0x07E0) >> 5
+        b = (color & 0x1F)
         buf = bytearray(w * 16)
         fbuf = FrameBuffer(buf, w, h, RGB565)
         if background != 0:
-            bg_r = (background & 0xF800) >> 8
-            bg_g = (background & 0x07E0) >> 3
-            bg_b = (background & 0x1F) << 3
+            bg_r = (background & 0xF800) >> 11
+            bg_g = (background & 0x07E0) >> 5
+            bg_b = (background & 0x1F)
             fbuf.fill(color565(bg_b, bg_r, bg_g))
         fbuf.text(text, 0, 0, color565(b, r, g))
         if rotate == 0:
